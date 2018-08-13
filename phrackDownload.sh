1d05cfd8e1b5dd1683a8948816dbfed97dc9cebf
Dylan Hudson <ddoghotdog@gmail.com>
2018-08-13 10:51:01 -0700

    Removed comments, its bash

--- phrackDownload.sh
+++ phrackDownload.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# We need this for later
 workingDirectory=$(pwd)
 echo "[+] We are currently in $workingDirectory"
 echo ""
@@ -8,23 +7,16 @@ sleep 3
 echo "[+] Making directory for Phrack Magazine"
 mkdir phrackMagazine
 echo "[+] Downloading All Phrack Archives"
-# Recursive, no parent directory, reject all index.html files and anything that follows index.html
 wget -r --no-parent --reject="index.html*" http://www.phrack.org/archives/tgz/
-# Getting the number of files we have downloaded
 numberOfFiles=$(ls -l $workingDirectory/www.phrack.org/archives/tgz | wc -l)
 fileCountActual=$(expr $numberOfFiles - 1)
-# Shows number of current issues downloaded
 echo ""
 echo "[+] Current number of issues: $fileCountActual" 
 echo ""
 echo "[+] Sleeping for 3 seconds..."
 sleep 3
-# Moving the files we got from Phrack and putting them in a new folder
 mv $workingDirectory/www.phrack.org/archives/tgz/* $workingDirectory/phrackMagazine/
-# Removing old file
 rm -rf $workingDirectory/www.phrack.org
-#cd $workingDirectory/phrackMagazine
-# Creating directories for the files to stay in
 for ((i=1; i<=fileCountActual; i++))
 do
 	DIRECTORY="phrack$i"
